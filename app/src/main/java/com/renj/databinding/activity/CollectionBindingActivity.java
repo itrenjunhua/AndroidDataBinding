0670b8b61fcf6258282f6177d37baf11f81a9395
Renj <itrenjunhua@163.com>
2018-11-02 10:15:26 +0800

    ObservableArrayList、ObservableArrayMap示例编写

--- app/src/main/java/com/renj/databinding/activity/CollectionBindingActivity.java
+++ app/src/main/java/com/renj/databinding/activity/CollectionBindingActivity.java
@@ -1,6 +1,8 @@
 package com.renj.databinding.activity;
 
 import android.databinding.DataBindingUtil;
+import android.databinding.ObservableArrayList;
+import android.databinding.ObservableArrayMap;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
@@ -25,9 +27,25 @@ import com.renj.databinding.databinding.ActivityBindingCollectionBinding;
 public class CollectionBindingActivity extends AppCompatActivity {
     private ActivityBindingCollectionBinding binding;
 
+    private final ObservableArrayList<String> observableArrayList = new ObservableArrayList<>();
+    private final ObservableArrayMap<String, String> observableArrayMap = new ObservableArrayMap<>();
+
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        binding = DataBindingUtil.setContentView(this,R.layout.activity_binding_collection);
+        binding = DataBindingUtil.setContentView(this, R.layout.activity_binding_collection);
+
+        observableArrayList.add("String Data -> 0");
+        observableArrayList.add("String Data -> 1");
+        observableArrayList.add("String Data -> 2");
+        observableArrayList.add("String Data -> 3");
+
+        observableArrayMap.put("key0", "value0");
+        observableArrayMap.put("key1", "value1");
+        observableArrayMap.put("key2", "value2");
+        observableArrayMap.put("key3", "value3");
+
+        binding.setObservableArrayList(observableArrayList);
+        binding.setObservableArrayMap(observableArrayMap);
     }
 }
